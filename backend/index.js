74a7dda97480e0ba12ddc727d581a5f3b6570bab
jamesjoy-2k24 <jamesjoy2k24@gmail.com>
2024-05-29 15:40:07 +0530

    updated connection uri

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
+//
 import express from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-import cookieParser from "cookie-parser";
 import helmet from "helmet";
 
 import authRoute from "./Routes/auth.js";
@@ -14,21 +13,50 @@ import playerRoute from "./Routes/player.js";
 import bookingRoute from "./Routes/booking.js";
 import reviewRoute from "./Routes/review.js";
 
-import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+const port = 5000 || process.env.PORT;
 
 const corsOptions = {
   origin: "http://localhost:3000",
-  credentials: true, // access-control-allow-credentials:true
+  credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/admin", adminRoute);
+app.use("/api/v1/players", playerRoute);
+app.use("/api/v1/bookings", bookingRoute);
+app.use("/api/v1/reviews", reviewRoute);
+
+// Database connection
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Successfully connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function startServer() {
+  await connectDB();
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+startServer();
 
 // Add helmet middleware before defining your routesapp.use(helmet({app.use(helmet({
 app.use(
@@ -63,46 +91,3 @@ app.use(
     },
   })
 );
-
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/users", userRoute);
-app.use("/api/v1/admin", adminRoute);
-app.use("/api/v1/players", playerRoute);
-app.use("/api/v1/bookings", bookingRoute);
-app.use("/api/v1/reviews", reviewRoute);
-
-// Database connection
-mongoose.set("strictQuery", false);
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB Successfully connected");
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-async function startServer() {
-  await connectDB();
-}
-
-startServer();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Serve static files from the React frontend app
-app.use(express.static(join(__dirname, "../frontend/dist")));
-
-// Anything that doesn't match the API routes should send the index.html from the frontend build
-app.get("*", (req, res) => {
-  res.sendFile(join(__dirname, "../frontend/dist", "index.html"));
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
