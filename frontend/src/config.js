4c35942b1efec65ace38cabbfa845bd1239b79b0
jamesjoy-2k24 <jamesjoy2k24@gmail.com>
2024-06-17 16:00:15 +0530

    changed port

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,5 @@
+/** @format */
+
 // export const BASE_URL = "https://pro-pulse.onrender.com/api/v1";
-export const BASE_URL = "http://localhost:5005/api/v1";
-export const token = localStorage.getItem("token");
+export const BASE_URL = 'http://localhost:5000/api/v1';
+export const token = localStorage.getItem('token');
